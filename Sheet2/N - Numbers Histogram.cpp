ac803b7b0181f932dabe423a67a20afc5bd69103
Mazen Tarek <mazentarek13@gmail.com>
2022-04-29 02:04:39 +0200

    Create N - Numbers Histogram.cpp

---
+++ Sheet2/N - Numbers Histogram.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+ 
+    char s;
+    int n;
+    cin >> s >> n;
+    for (int i = 1 ; i <= n ; i++){
+        int ni;
+        cin >> ni;
+        for (int j = 1 ; j <= ni ; j++){
+            cout << s;
+        }
+        cout << endl;
+    }
+ 
+return 0;
+}
