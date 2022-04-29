c446c4506f451a475aa4c47268ee712b5b86d6b2
Mazen Tarek <mazentarek13@gmail.com>
2022-04-29 02:07:43 +0200

    Create T - Shape2.cpp

---
+++ Sheet2/T - Shape2.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+ 
+    int n;
+    cin >> n;
+    for(int i = 1, k = 0; i <= n; ++i, k = 0){
+        for(int j = 1; j <= n-i; ++j){
+            cout <<" ";
+        }
+        while(k != 2*i-1)
+        {
+            cout << "*";
+            ++k;
+        }
+        cout << endl;
+    }
+ 
+return 0;
+}
