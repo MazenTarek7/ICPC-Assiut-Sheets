bc6f3b00a074534bb50bbf8d869099f18431f5b2
Mazen Tarek <mazentarek13@gmail.com>
2022-04-29 02:01:22 +0200

    Create E - Max.cpp

---
+++ Sheet2/E - Max.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+ 
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    sort(arr , arr + n, greater<int>());
+    cout << arr[0] << endl;
+ 
+ 
+ 
+return 0;
+}
